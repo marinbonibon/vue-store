c1f1d3132e0338774dffcfa6e1f0f38bfbd0dd72
Maryna Petrenko <marinbonibon@gmail.com>
2020-10-06 20:22:18 +0300

    implement cart page

--- src/components/User/Checkout.vue
+++ src/components/User/Checkout.vue
@@ -1,8 +1,29 @@
 <template>
   <v-container>
-    <v-layout>
-      <v-flex>
-        <h1>Checkout</h1>
+    <v-layout row>
+      <v-flex sx12 sm6 offset-sm-3>
+        <h1 class="text-secondary mb-3">Orders</h1>
+        <v-list-item-group
+          multiple
+        >
+          <v-list-item v-for="(order, i) of orders" :key="i">
+            <template>
+              <v-list-item-action>
+                <v-checkbox
+                  color="cyan"
+                  :input-value="order.done"
+                  @change="markDone"
+                ></v-checkbox>
+              </v-list-item-action>
+
+              <v-list-item-content>
+                <v-list-item-title>{{order.name}}</v-list-item-title>
+                <v-list-item-subtitle>{{order.phone}}</v-list-item-subtitle>
+              </v-list-item-content>
+              <v-btn color="cyan" dark :to="`/product/${order.productId}`">Open</v-btn>
+            </template>
+          </v-list-item>
+        </v-list-item-group>
       </v-flex>
     </v-layout>
   </v-container>
@@ -10,7 +31,21 @@
 <script>
 export default {
   data() {
-    return {};
+    return {
+      orders: [{
+        id: 'test',
+        name: 'Marina',
+        phone: '12-23-34',
+        productId: '1',
+        done: false,
+      }],
+    };
+  },
+  methods: {
+    markDone(order) {
+      // eslint-disable-next-line no-param-reassign
+      order.done = true;
+    },
   },
 };
 </script>
